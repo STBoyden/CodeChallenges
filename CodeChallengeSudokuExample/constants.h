838a522477088bf8fe9a62a0f868b883ad2e57a6
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-09-12 20:44:58 -0700

    :ambulance: oops, removed constexpr

--- CodeChallengeSudokuExample/constants.h
+++ CodeChallengeSudokuExample/constants.h
@@ -1,33 +1,33 @@
 #pragma once
 struct
 {
-    constexpr int BOARD_HEIGHT = 9;
-    constexpr int BOARD_WIDTH = 9;
-    constexpr int CLIQUE_HEIGHT = 3;
-    constexpr int CLIQUE_WIDTH = 3;
-    constexpr int NUM_CLIQUES = (BOARD_HEIGHT * BOARD_WIDTH); // (CLIQUE_HEIGHT * CLIQUE_WIDTH)
+    int BOARD_HEIGHT = 9;
+    int BOARD_WIDTH = 9;
+    int CLIQUE_HEIGHT = 3;
+    int CLIQUE_WIDTH = 3;
+    int NUM_CLIQUES = (BOARD_HEIGHT * BOARD_WIDTH); // (CLIQUE_HEIGHT * CLIQUE_WIDTH)
 
     struct
     {
-        constexpr char DOUBLE_VERTICAL = "║";
-        constexpr char DOUBLE_HORIZONTAL = "═";
-        constexpr char DOUBLE_NE = "╗";
-        constexpr char DOUBLE_NW = "╔";
-        constexpr char DOUBLE_SW = "╚";
-        constexpr char DOUBLE_SE = "╝";
-        constexpr char DOUBLE_SINGLE_S = "╤";
-        constexpr char DOUBLE_SINGLE_E = "╟";
-        constexpr char DOUBLE_SINGLE_N = "╧";
-        constexpr char DOUBLE_SINGLE_W = "╢";
-        constexpr char SINGLE_VERTICAL = "│";
-        constexpr char SINGLE_HORIZONTAL = "─";
-        constexpr char SINGLE_CROSS = "┼";
+        std::string DOUBLE_VERTICAL = "║";
+        std::string DOUBLE_HORIZONTAL = "═";
+        std::string DOUBLE_NE = "╗";
+        std::string DOUBLE_NW = "╔";
+        std::string DOUBLE_SW = "╚";
+        std::string DOUBLE_SE = "╝";
+        std::string DOUBLE_SINGLE_S = "╤";
+        std::string DOUBLE_SINGLE_E = "╟";
+        std::string DOUBLE_SINGLE_N = "╧";
+        std::string DOUBLE_SINGLE_W = "╢";
+        std::string SINGLE_VERTICAL = "│";
+        std::string SINGLE_HORIZONTAL = "─";
+        std::string SINGLE_CROSS = "┼";
     } Border;
 
     struct
     {
-        constexpr float SLEEP_DURATION = 0.001;
-        constexpr int BOARD_HEIGHT = 13;
-        constexpr int CLIQUE_SIZE = 7;
+        float SLEEP_DURATION = 0.001;
+        int BOARD_HEIGHT = 13;
+        int CLIQUE_SIZE = 7;
     } Printing;
 } Constants;
