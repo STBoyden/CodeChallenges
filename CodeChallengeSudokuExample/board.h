529f0ae3273b855e2594d64735c30295563d4919
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-09-12 20:44:07 -0700

    :racehorse: changed editable_indices to map

--- CodeChallengeSudokuExample/board.h
+++ CodeChallengeSudokuExample/board.h
@@ -2,30 +2,36 @@
 #include "constants.h"
 #include "min_mat.h"
 #include "point.h"
-#include <vector>
+#include <unordered_map>
 
 class Board
 {
 private:
     Mat<uint_fast8_t> raw_table;
-    std::vector<Point<2, uint_fast8_t>> editable_indices;
+    std::unordered_map<Point<2, uint_fast8_t>, size_t> editable_indices;
 
 public:
-    Board(const Mat<uint_fast8_t> &raw_table)
+    Board() = delete;
+    Board(Mat<uint_fast8_t> &&raw_table)
     {
-        if (m.SizeY() != Constants.BOARD_HEIGHT)
+        if (raw_table.SizeY() != Constants.BOARD_HEIGHT)
             throw std::invalid_argument("Raw Table height is incorrect!\n\n" + raw_table.ToString());
 
-        if (m.SizeX() != Constants.BOARD_WIDTH)
+        if (raw_table.SizeX() != Constants.BOARD_WIDTH)
             throw std::invalid_argument("Raw Table width is incorrect!\n\n" + raw_table.ToString());
 
-        this.raw_table = std::move(raw_table);
+        this->raw_table = std::move(raw_table);
 
-        for (int i = 0; i < Constants.BOARD_HEIGHT; ++i)
-            for (int j = 0; j < Constants.BOARD_WIDTH; ++j)
+        Point<2, uint_fast8_t> temp;
+
+        for (uint_fast8_t i = 0; i < Constants.BOARD_HEIGHT; ++i)
+            for (uint_fast8_t j = 0; j < Constants.BOARD_WIDTH; ++j)
             {
                 if (!raw_table.At(j, i))
-                    editable_indices.push_back(Point<2, uint_fast8_t>(j, i));
+                {
+                    temp = std::move(Point<2, uint_fast8_t>({j, i}));
+                    editable_indices.insert(std::make_pair(temp, std::hash<Point<2, uint_fast8_t>>{}(temp)));
+                }
             }
     }
-}
+};
