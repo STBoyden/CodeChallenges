10a5a6f84866be4e60fd265a96d19f14b2fe287c
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-09-15 11:39:12 -0700

    :racehorse: re-structured functions for performance

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <chrono>
+#include <iomanip>
 #include "board.h"
 #include "tpool.h"
 
@@ -20,25 +21,30 @@ inline void UpdateUnsolved(SudokuBoard &s)
     s.unsolved = std::move(res);
 }
 
-// //branchless
-// inline Point<2, int> GetLocal(const Point<2, int> &p)
-// {
-//     Point<2, int> res{p[0], p[1]};
-
-//     res[0] -= 2 * ((res[0] + 1) % 3 == 0);
-//     res[0] -= (res[0] + 2) % 3 == 0;
-
-//     res[1] -= 2 * ((res[1] + 1) % 3 == 0);
-//     res[1] -= (res[1] + 2) % 3 == 0;
-
-//     return res;
-// }
-
-inline bool TrimCanidates(SudokuBoard &s)
+inline void TrimCanidates(SudokuBoard &s)
 {
-    int res = false;
     Point<2, int> localPoint;
 
+    //triming local
+    for (int i = 0; i < s.local.SizeY(); ++i)
+    {
+        for (int j = 0; j < s.local.SizeX(); ++j)
+        {
+            for (int y = 0; y < 3; ++y)
+            {
+                for (int x = 0; x < 3; ++x)
+                {
+                    s.local.At(j, i).Erase(s.board.At((j * 3) + x, (i * 3) + y).Front() * s.board.At((j * 3) + x, (i * 3) + y).Solved()); //erasing solved bits
+                    for (int k = 0; k < 10; ++k)
+                    {
+                        s.board.At((j * 3) + x, (i * 3) + y).Erase((!s.local.At(j, i).Contains(k) * k) * !s.board.At((j * 3) + x, (i * 3) + y).Solved());
+                    }
+                }
+            }
+        }
+    }
+
+    //trimming individual
     for (const Point<2, int> &p : s.unsolved)
     {
         //checking vertical and horizontal
@@ -58,33 +64,8 @@ inline bool TrimCanidates(SudokuBoard &s)
         {
             s.board.At(p[0], p[1]).Erase(s.board.At(i, p[1]).Front() * s.board.At(i, p[1]).Solved());
         }
-
-        res += s.board.At(p[0], p[1]).Solved(); //branchless
     }
-
-    return res;
-}
-
-inline void TrimLocalCanidates(SudokuBoard &s)
-{
-    int res = 0;
-    for (int i = 0; i < s.local.SizeY(); ++i)
-    {
-        for (int j = 0; j < s.local.SizeX(); ++j)
-        {
-            for (int y = 0; y < 3; ++y)
-            {
-                for (int x = 0; x < 3; ++x)
-                {
-                    s.local.At(j, i).Erase(s.board.At((j * 3) + x, (i * 3) + y).Front() * s.board.At((j * 3) + x, (i * 3) + y).Solved()); //erasing solved bits
-                    for (int k = 0; k < 10; ++k)
-                    {
-                        s.board.At((j * 3) + x, (i * 3) + y).Erase((!s.local.At(j, i).Contains(k) * k) * !s.board.At((j * 3) + x, (i * 3) + y).Solved());
-                    }
-                }
-            }
-        }
-    }
+    UpdateUnsolved(s);
 }
 
 int main(int argc, char *argv[])
@@ -92,15 +73,13 @@ int main(int argc, char *argv[])
     using namespace std::chrono;
 
     // std::vector<int> cells;
-    // std::string a = argv[3];
+    // std::string a = argv[1];
     // std::stringstream ss(a);
     // std::string line;
     // while (std::getline(ss, line, ','))
     // {
     //     cells.push_back(std::stoi(line));
     // }
-
-    //std::stoi(argv[1]), std::stoi(argv[2])
 
     //   {7, 2, 0,     0, 9, 6,    0, 0, 3,
     //    0, 0, 0,     2, 0, 5,    0, 0, 0,
@@ -124,23 +103,27 @@ int main(int argc, char *argv[])
                            0, 0, 0, 7, 0, 2, 0, 0, 0,
                            2, 0, 0, 4, 3, 0, 0, 1, 8});
 
+    int x = gameBoard.unsolved.size();
+
     //start timer
     high_resolution_clock::time_point t1 = high_resolution_clock::now();
     {
-        //zoomMachine.Start();
-        while (gameBoard.unsolved.size()) //while not solved
+        while (x) //while not solved
         {
         start:
+            std::cout << gameBoard.board << '\n'
+                      << gameBoard.local << '\n';
+
             UpdateUnsolved(gameBoard);
 
             //strats
-            TrimLocalCanidates(gameBoard);
-            if (TrimCanidates(gameBoard))
+            TrimCanidates(gameBoard);
+            if (x != gameBoard.unsolved.size())
+            {
+                x = gameBoard.unsolved.size();
                 goto start;
-
+            }
             break;
-
-            //std::cout << "Cells Left: " << gameBoard.unsolved.size() << '\n';
         }
     }
     high_resolution_clock::time_point t2 = high_resolution_clock::now();
@@ -149,9 +132,14 @@ int main(int argc, char *argv[])
     //end timer
     std::cout << gameBoard.unsolved.size() << '\n';
     std::cout << "wowee solved!\n";
-    std::cout << time_span.count() << '\n';
+    std::cout << std::setprecision(7) << std::fixed << time_span.count() << '\n';
     std::cout << gameBoard.board << '\n'
               << gameBoard.local << '\n';
+
+    //function call count
+    // erase - 6412
+    // front - 3622
+    // turnon - 586
 
     return 0;
 }
