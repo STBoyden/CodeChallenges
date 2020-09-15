658dc965c3e7a46eca8f50e733ab9a48eae67d21
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-09-14 21:18:02 -0700

    :construction: Triming of LocalCanidates Added

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -20,23 +20,25 @@ inline void UpdateUnsolved(SudokuBoard &s)
     s.unsolved = std::move(res);
 }
 
-//branchless
-inline Point<2, int> GetLocal(const Point<2, int> &p)
-{
-    Point<2, int> res{p[0], p[1]};
+// //branchless
+// inline Point<2, int> GetLocal(const Point<2, int> &p)
+// {
+//     Point<2, int> res{p[0], p[1]};
 
-    res[0] -= 2 * ((res[0] + 1) % 3 == 0);
-    res[0] -= (res[0] + 2) % 3 == 0;
+//     res[0] -= 2 * ((res[0] + 1) % 3 == 0);
+//     res[0] -= (res[0] + 2) % 3 == 0;
 
-    res[1] -= 2 * ((res[1] + 1) % 3 == 0);
-    res[1] -= (res[1] + 2) % 3 == 0;
+//     res[1] -= 2 * ((res[1] + 1) % 3 == 0);
+//     res[1] -= (res[1] + 2) % 3 == 0;
 
-    return res;
-}
+//     return res;
+// }
 
 inline bool TrimCanidates(SudokuBoard &s)
 {
     int res = false;
+    Point<2, int> localPoint;
+
     for (const Point<2, int> &p : s.unsolved)
     {
         //checking vertical and horizontal
@@ -58,11 +60,31 @@ inline bool TrimCanidates(SudokuBoard &s)
         }
 
         res += s.board.At(p[0], p[1]).Solved(); //branchless
-
-        //local group
     }
 
     return res;
+}
+
+inline void TrimLocalCanidates(SudokuBoard &s)
+{
+    int res = 0;
+    for (int i = 0; i < s.local.SizeY(); ++i)
+    {
+        for (int j = 0; j < s.local.SizeX(); ++j)
+        {
+            for (int y = 0; y < 3; ++y)
+            {
+                for (int x = 0; x < 3; ++x)
+                {
+                    s.local.At(j, i).Erase(s.board.At((j * 3) + x, (i * 3) + y).Front() * s.board.At((j * 3) + x, (i * 3) + y).Solved()); //erasing solved bits
+                    for (int k = 0; k < 10; ++k)
+                    {
+                        s.board.At((j * 3) + x, (i * 3) + y).Erase((!s.local.At(j, i).Contains(k) * k) * !s.board.At((j * 3) + x, (i * 3) + y).Solved());
+                    }
+                }
+            }
+        }
+    }
 }
 
 int main(int argc, char *argv[])
@@ -92,7 +114,6 @@ int main(int argc, char *argv[])
     //    0, 0, 0,     7, 0, 2,    0, 0, 0,
     //    2, 0, 0,     4, 3, 0,    0, 1, 8});
 
-    ThreadPool zoomMachine;
     SudokuBoard gameBoard({7, 2, 0, 0, 9, 6, 0, 0, 3,
                            0, 0, 0, 2, 0, 5, 0, 0, 0,
                            0, 8, 0, 0, 0, 4, 0, 2, 0,
@@ -104,7 +125,6 @@ int main(int argc, char *argv[])
                            2, 0, 0, 4, 3, 0, 0, 1, 8});
 
     //start timer
-    std::cout << GetLocal({4, 5}) << '\n';
     high_resolution_clock::time_point t1 = high_resolution_clock::now();
     {
         //zoomMachine.Start();
@@ -112,9 +132,13 @@ int main(int argc, char *argv[])
         {
         start:
             UpdateUnsolved(gameBoard);
+
             //strats
+            TrimLocalCanidates(gameBoard);
             if (TrimCanidates(gameBoard))
                 goto start;
+
+            break;
 
             //std::cout << "Cells Left: " << gameBoard.unsolved.size() << '\n';
         }
@@ -126,7 +150,8 @@ int main(int argc, char *argv[])
     std::cout << gameBoard.unsolved.size() << '\n';
     std::cout << "wowee solved!\n";
     std::cout << time_span.count() << '\n';
-    std::cout << gameBoard.board << '\n';
+    std::cout << gameBoard.board << '\n'
+              << gameBoard.local << '\n';
 
     return 0;
 }
