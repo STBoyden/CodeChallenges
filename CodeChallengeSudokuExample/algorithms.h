896181bbe82ead4291984ec15766e3f69cebe687
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-09-18 13:22:38 -0700

    :construction: Implemented Basic Backtracking
    although as is, is super slow for some reason, so ill need to work on this further

--- CodeChallengeSudokuExample/algorithms.h
+++ CodeChallengeSudokuExample/algorithms.h
@@ -1,7 +1,36 @@
 #pragma once
+#include <chrono>
+#include "board.h"
 
-class BaseAlgorithm
+namespace BasicRecursiveBacktrackAlgorithm
 {
-public:
-    virtual static void solve(board, print_func) = 0;
-};
+    bool basic_recursive_backtrack_solve(Board &b, int editable_index = 0)
+    {
+        if (editable_index >= b.editable_indices_size())
+            return b.is_board_valid();
+
+        Point<2, int> p = b.editable_indices[editable_index];
+        for (int attempt = 1; attempt <= Constants::BOARD_HEIGHT; ++attempt)
+        {
+            b.raw_table.At(p[0], p[1]) = attempt;
+
+            if (!b.is_square_valid(editable_index))
+                continue;
+
+            if (BasicRecursiveBacktrackAlgorithm::basic_recursive_backtrack_solve(b, editable_index + 1))
+                return true;
+        }
+
+        b.raw_table.At(p[0], p[1]) = 0;
+
+        return false;
+    }
+
+    inline double solve(Board &b)
+    {
+        std::chrono::high_resolution_clock::time_point time_start = std::chrono::high_resolution_clock::now();
+        BasicRecursiveBacktrackAlgorithm::basic_recursive_backtrack_solve(b);
+        std::chrono::high_resolution_clock::time_point time_end = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration_cast<std::chrono::duration<double>>(time_end - time_start).count();
+    }
+}; // namespace BasicRecursiveBacktrackAlgorithm
