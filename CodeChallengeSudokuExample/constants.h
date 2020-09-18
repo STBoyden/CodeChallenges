06399f0536a7dc7a38d373af6fb5c93d07e9f416
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-09-17 17:33:07 -0700

    :bug: fixed constants

--- CodeChallengeSudokuExample/constants.h
+++ CodeChallengeSudokuExample/constants.h
@@ -1,33 +1,33 @@
 #pragma once
 struct
 {
-    int BOARD_HEIGHT = 9;
-    int BOARD_WIDTH = 9;
-    int CLIQUE_HEIGHT = 3;
-    int CLIQUE_WIDTH = 3;
-    int NUM_CLIQUES = (BOARD_HEIGHT * BOARD_WIDTH); // (CLIQUE_HEIGHT * CLIQUE_WIDTH)
+    const int BOARD_HEIGHT = 9;
+    const int BOARD_WIDTH = 9;
+    const int CLIQUE_HEIGHT = 3;
+    const int CLIQUE_WIDTH = 3;
+    const int NUM_CLIQUES = (BOARD_HEIGHT * BOARD_WIDTH); // (CLIQUE_HEIGHT * CLIQUE_WIDTH)
 
     struct
     {
-        std::string DOUBLE_VERTICAL = "║";
-        std::string DOUBLE_HORIZONTAL = "═";
-        std::string DOUBLE_NE = "╗";
-        std::string DOUBLE_NW = "╔";
-        std::string DOUBLE_SW = "╚";
-        std::string DOUBLE_SE = "╝";
-        std::string DOUBLE_SINGLE_S = "╤";
-        std::string DOUBLE_SINGLE_E = "╟";
-        std::string DOUBLE_SINGLE_N = "╧";
-        std::string DOUBLE_SINGLE_W = "╢";
-        std::string SINGLE_VERTICAL = "│";
-        std::string SINGLE_HORIZONTAL = "─";
-        std::string SINGLE_CROSS = "┼";
+        const char DOUBLE_VERTICAL = 186;
+        const char DOUBLE_HORIZONTAL = 205;
+        const char DOUBLE_NE = 187;
+        const char DOUBLE_NW = 201;
+        const char DOUBLE_SW = 200;
+        const char DOUBLE_SE = 188;
+        const char DOUBLE_SINGLE_S = 203;
+        const char DOUBLE_SINGLE_E = 204;
+        const char DOUBLE_SINGLE_N = 202;
+        const char DOUBLE_SINGLE_W = 185;
+        const char SINGLE_VERTICAL = 186;
+        const char SINGLE_HORIZONTAL = 205;
+        const char SINGLE_CROSS = 206;
     } Border;
 
     struct
     {
-        float SLEEP_DURATION = 0.001;
-        int BOARD_HEIGHT = 13;
-        int CLIQUE_SIZE = 7;
+        const float SLEEP_DURATION = 0.001;
+        const int BOARD_HEIGHT = 13;
+        const int CLIQUE_SIZE = 7;
     } Printing;
 } Constants;
