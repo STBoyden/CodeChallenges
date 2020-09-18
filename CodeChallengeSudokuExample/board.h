4214807fa94873265e21e9a3a8a9b31c647c8d4e
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-09-17 17:30:59 -0700

    Finished board.py equivelant

--- CodeChallengeSudokuExample/board.h
+++ CodeChallengeSudokuExample/board.h
@@ -2,7 +2,9 @@
 #include "constants.h"
 #include "min_mat.h"
 #include "point.h"
+#include "helpers.h"
 #include <vector>
+#include <set>
 
 class Board
 {
@@ -12,7 +14,7 @@ private:
 
 public:
     Board() = delete;
-    Board(Mat<uint_fast8_t> &&raw_table)
+    Board(const Mat<uint_fast8_t> &raw_table)
     {
         if (raw_table.SizeY() != Constants.BOARD_HEIGHT)
             throw std::invalid_argument("Raw Table height is incorrect!\n\n" + raw_table.ToString());
@@ -25,12 +27,172 @@ public:
         Point<2, uint_fast8_t> temp;
 
         for (uint_fast8_t i = 0; i < Constants.BOARD_HEIGHT; ++i)
+        {
             for (uint_fast8_t j = 0; j < Constants.BOARD_WIDTH; ++j)
             {
                 if (!raw_table.At(j, i))
-                {
                     editable_indices.push_back(Point<2, uint_fast8_t>({j, i}));
-                }
             }
+        }
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Board &b)
+    {
+        os << Constants.Border.DOUBLE_NW;
+        for (int j = 0; j < 2; ++j)
+        {
+            for (int i = 0; i < Constants.Printing.CLIQUE_SIZE; ++i)
+            {
+                os << Constants.Border.DOUBLE_HORIZONTAL;
+            }
+            os << Constants.Border.DOUBLE_SINGLE_S;
+        }
+        for (int i = 0; i < Constants.Printing.CLIQUE_SIZE; ++i)
+        {
+            os << Constants.Border.DOUBLE_HORIZONTAL;
+        }
+        os << Constants.Border.DOUBLE_NE << '\n';
+
+        int real_y, real_x;
+        for (int y1 = 0; y1 < Constants.BOARD_HEIGHT / Constants.CLIQUE_HEIGHT; ++y1)
+        {
+            for (int y2 = 0; y2 < Constants.CLIQUE_HEIGHT; ++y2)
+            {
+                real_y = (y1 * Constants.CLIQUE_HEIGHT) + y2;
+                os << Constants.Border.DOUBLE_VERTICAL << ' ';
+                for (int x1 = 0; x1 < Constants.BOARD_WIDTH / Constants.CLIQUE_WIDTH; ++x1)
+                {
+                    for (int x2 = 0; x2 < Constants.CLIQUE_WIDTH; ++x2)
+                    {
+                        real_x = (x1 * Constants.CLIQUE_WIDTH) + x2;
+                        os << (int)b.raw_table.At(real_x, real_y) << ' ';
+                    }
+                    os << Constants.Border.SINGLE_VERTICAL << ' ';
+                }
+                os << "\n";
+            }
+            if (y1 != Constants.BOARD_HEIGHT / Constants.CLIQUE_HEIGHT - 1)
+            {
+                os << Constants.Border.DOUBLE_SINGLE_E;
+                for (int i = 0; i < 2; ++i)
+                {
+                    for (int j = 0; j < Constants.Printing.CLIQUE_SIZE; ++j)
+                    {
+                        os << Constants.Border.SINGLE_HORIZONTAL;
+                    }
+                    os << Constants.Border.SINGLE_CROSS;
+                }
+                for (int j = 0; j < Constants.Printing.CLIQUE_SIZE; ++j)
+                {
+                    os << Constants.Border.SINGLE_HORIZONTAL;
+                }
+                os << Constants.Border.DOUBLE_SINGLE_W << '\n';
+            }
+        }
+
+        os << Constants.Border.DOUBLE_SW;
+        for (int j = 0; j < 2; ++j)
+        {
+            for (int i = 0; i < Constants.Printing.CLIQUE_SIZE; ++i)
+            {
+                os << Constants.Border.DOUBLE_HORIZONTAL;
+            }
+            os << Constants.Border.DOUBLE_SINGLE_N;
+        }
+        for (int i = 0; i < Constants.Printing.CLIQUE_SIZE; ++i)
+        {
+            os << Constants.Border.DOUBLE_HORIZONTAL;
+        }
+        os << Constants.Border.DOUBLE_SE;
+
+        return os;
+    }
+
+    size_t editable_indices_size() const
+    {
+        return editable_indices.size();
+    }
+
+    //changed from dict in py to vec in c++... I couldnt help myself, O(1) vs O(log n) and all
+    bool is_colum_valid(int x) const
+    {
+        std::vector<bool> occurrences_vec{0, 0, 0, 0, 0, 0, 0, 0, 0}; //in py prog this is a dict, which is O(log n), but as vec its just O(1)
+        int square_value = 0;
+        for (int y = 0; y < Constants.BOARD_HEIGHT; ++y)
+        {
+            square_value = raw_table.At(x, y);
+
+            if (!square_value)
+                continue;
+            if (occurrences_vec[square_value - 1]) //does it contain
+                return false;
+            else
+                occurrences_vec[square_value - 1] = true; //now it contains it
+        }
+        return true;
+    }
+
+    bool is_row_valid(int y) const
+    {
+        std::vector<bool> occurrences_vec{0, 0, 0, 0, 0, 0, 0, 0, 0}; //in py prog this is a dict, which is O(log n), but as vec its just O(1)
+        uint_fast8_t square_value;
+        for (int x = 0; x < Constants.BOARD_HEIGHT; ++x)
+        {
+            square_value = raw_table.At(x, y);
+
+            if (!square_value)
+                continue;
+            if (occurrences_vec[square_value - 1]) //does it contain
+                return false;
+            else
+                occurrences_vec[square_value - 1] = true; //now it contains it
+        }
+        return true;
+    }
+
+    bool is_clique_valid(const Point<2, int> &cliqueStart) const
+    {
+        std::vector<bool> occurrences_vec{0, 0, 0, 0, 0, 0, 0, 0, 0}; //in py prog this is a dict, which is O(log n), but as vec its just O(1)
+        uint_fast8_t square_value;
+        Point<2, int> local = BoardHelper::GetLocal(cliqueStart);
+
+        for (int y = 0; y < Constants.CLIQUE_HEIGHT; ++y)
+        {
+            for (int x = 0; x < Constants.BOARD_WIDTH; ++x)
+            {
+                square_value = raw_table.At(local[0] + x, local[1] + y);
+
+                if (!square_value)
+                    continue;
+
+                if (occurrences_vec[square_value - 1])
+                    return false;
+                else
+                    occurrences_vec[square_value - 1] = 1;
+            }
+        }
+
+        return true;
+    }
+    bool is_board_valid() const
+    {
+        for (int y = 0; y < Constants.BOARD_HEIGHT; ++y)
+        {
+            if (!is_row_valid(y))
+                return false;
+        }
+        for (int x = 0; x < Constants.BOARD_WIDTH; ++x)
+        {
+            if (!is_colum_valid(x))
+                return false;
+        }
+        for (int i = 0; i < Constants.BOARD_HEIGHT; i += 3)
+        {
+            for (int j = 0; j < Constants.BOARD_WIDTH; j += 3)
+            {
+                if (!is_clique_valid(Point<2, int>{j, i}))
+                    return false;
+            }
+        }
     }
 };
