9d177ffce12c0cd063e18dc971380c6efaa35e82
EntireTwix <31377881+EntireTwix@users.noreply.github.com>
2020-09-19 12:10:51 -0700

    :tada: added user input of board

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -85,18 +85,21 @@ bool BasicBacktrack(SudokuBoard &b, Mat<Canidates> &res)
     return false;
 }
 
-int main(int argc, char *argv[])
+int main()
 {
     using namespace std::chrono;
 
-    // std::vector<int> cells;
-    // std::string a = argv[1];
-    // std::stringstream ss(a);
-    // std::string line;
-    // while (std::getline(ss, line, ','))
-    // {
-    //     cells.push_back(std::stoi(line));
-    // }
+    std::vector<int> cells;
+    std::string line;
+    for (int i = 0; i < 9; ++i)
+    {
+        std::getline(std::cin, line);
+        std::stringstream ss(line);
+        while (std::getline(ss, line, ' '))
+        {
+            cells.push_back(std::stoi(line));
+        }
+    }
 
     //   {7, 2, 0,     0, 9, 6,    0, 0, 3,
     //    0, 0, 0,     2, 0, 5,    0, 0, 0,
@@ -110,15 +113,7 @@ int main(int argc, char *argv[])
     //    0, 0, 0,     7, 0, 2,    0, 0, 0,
     //    2, 0, 0,     4, 3, 0,    0, 1, 8});
 
-    SudokuBoard gameBoard({1, 0, 0, 4, 8, 9, 0, 0, 6,
-                           7, 3, 0, 0, 0, 0, 0, 4, 0,
-                           0, 0, 0, 0, 0, 1, 2, 9, 5,
-                           0, 0, 7, 1, 2, 0, 6, 0, 0,
-                           5, 0, 0, 7, 0, 3, 0, 0, 8,
-                           0, 0, 6, 0, 9, 5, 7, 0, 0,
-                           9, 1, 4, 6, 0, 0, 0, 0, 0,
-                           0, 2, 0, 0, 0, 0, 0, 3, 7,
-                           8, 0, 0, 5, 1, 2, 0, 0, 4});
+    SudokuBoard gameBoard(cells);
 
     int x = gameBoard.unsolved.size();
 
